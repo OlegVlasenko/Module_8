8cf947f7d0b3da94c7b1d37d5818abcc4a8df747
OlegVlasenko <vlasenko.inbox@gmail.com>
2016-05-02 16:33:04 +0300

    Change sort Method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,65 +1,24 @@
 package com.company;
 
-import com.company.FileDirectory.Jpeg;
-import com.company.FileDirectory.Mp3;
-import com.company.FileDirectory.Txt;
-import com.company.FlowerShop.Aster;
-import com.company.FlowerShop.Charmomile;
-import com.company.FlowerShop.Rose;
-import com.company.FlowerShop.Tulip;
-import com.company.MusicShop.Guitar;
-import com.company.MusicShop.Piano;
-import com.company.MusicShop.Trumpet;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Object> Gifts = new ArrayList<>();
+        ArrayList<Object> Gifts = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
+        Printer.initArrayRandom(Gifts, 25);
 
-            int choose = (int)(Math.random()*10);
-            switch (choose){
-                case 0:
-                    Gifts.add(new Jpeg("Name"+(int)(Math.random()*10)));
-                    break;
-                case 1:
-                    Gifts.add(new Mp3("Name"+(int)(Math.random()*10)));
-                    break;
-                case 2:
-                    Gifts.add(new Txt("Name"+(int)(Math.random()*10)));
-                    break;
-                case 3:
-                    Gifts.add(new Aster());
-                    break;
-                case 4:
-                    Gifts.add(new Charmomile());
-                    break;
-                case 5:
-                    Gifts.add(new Rose());
-                    break;
-                case 6:
-                    Gifts.add(new Tulip());
-                    break;
-                case 7:
-                    Gifts.add(new Guitar());
-                    break;
-                case 8:
-                    Gifts.add(new Piano());
-                    break;
-                case 9:
-                    Gifts.add(new Trumpet());
-                    break;
-                default:
-                    System.out.println("cant find " + choose);
-            }
-        }
+        //for (Object Item: Gifts ) System.out.println("Obj : " + Item);
 
-        for (Object Item: Gifts ) System.out.println("Obj : " + Item);
+        System.out.println("Random ArrayList before sort :");
+        //Задание 1
+        Printer.printClass(Gifts);
+
+        System.out.println("\nSame ArrayList after sort :");
+        //Задание 2
+        Printer.sort(Gifts);
 
         Printer.printClass(Gifts);
 
--- src/com/company/Printer.java
+++ src/com/company/Printer.java
@@ -1,47 +1,122 @@
 package com.company;
 
 import com.company.FileDirectory.File;
-import com.company.FlowerShop.Flower;
+import com.company.FileDirectory.Jpeg;
+import com.company.FileDirectory.Mp3;
+import com.company.FileDirectory.Txt;
+import com.company.FlowerShop.*;
+import com.company.MusicShop.Guitar;
 import com.company.MusicShop.MusicInstrument;
+import com.company.MusicShop.Piano;
+import com.company.MusicShop.Trumpet;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Printer {
 
-     private static String getName(Object param){
-        String choose = param.getClass().getName().replaceAll("com.company.","");
-        choose = choose.substring(0,choose.indexOf("."));
-        switch(choose){
+    //identifying class of object using 'instanceof'
+    private static String getName2(Object param) {
+        if (param instanceof Flower) return ((Flower) param).getName();
+        else if (param instanceof File) return ((File) param).getName();
+        else if (param instanceof MusicInstrument) return ((MusicInstrument) param).getName();
+        return "";
+    }
+
+    //identifying class of object using 'getClass().getName()'
+    private static String getName(Object param) {
+        String choose = param.getClass().getName().replaceAll("com.company.", "");
+        choose = choose.substring(0, choose.indexOf("."));
+        switch (choose) {
             case "FileDirectory":
-                return ((File)param).getName();
+                return ((File) param).getName();
             case "FlowerShop":
-                return ((Flower)param).getName();
-             case "MusicShop":
-                return ((MusicInstrument)param).getName();
+                return ((Flower) param).getName();
+            case "MusicShop":
+                return ((MusicInstrument) param).getName();
         }
         return "";
     }
 
-    private static String getAdditional(Object param){
-        String choose = param.getClass().getName().replaceAll("com.company.","");
-        choose = choose.substring(0,choose.indexOf("."));
-        switch(choose){
+    private static String getAdditional(Object param) {
+        String choose = param.getClass().getName().replaceAll("com.company.", "");
+        choose = choose.substring(0, choose.indexOf("."));
+        switch (choose) {
             case "FileDirectory":
-            return ((File) param).getExtension();
+                return ((File) param).getExtension();
         }
         return "";
     }
-
 
     public static void printClass(List<Object> paramList) {
         int counter = 1;
 
-        System.out.println("+---+------------+---+");
-        System.out.println("+ # +Name        +ext+");
-        System.out.println("+---+------------+---+");
+        System.out.println("+---+------------+---+------------+");
+        System.out.println("+ # +Name        +ext+ HashCode() !");
+        System.out.println("+---+------------+---+------------+");
         for (Object Item : paramList) {
-            System.out.printf("!%3d!%12s!%3s!\n",counter++, getName(Item),getAdditional(Item));
+            System.out.printf("!%3d!%12s!%3s!%11s !\n", counter++, getName(Item), getAdditional(Item), Item.getClass().hashCode());
         }
-        System.out.println("+---+------------+---+");
+        System.out.println("+---+------------+---+------------!");
+    }
+
+    static void sort(ArrayList<Object> paramArray) {
+
+        boolean needSort;
+        Object temp;
+
+        do {
+            needSort = false;
+            for (int i = 0; i < paramArray.size() - 1; i++) {
+                if (getName2(paramArray.get(i)).compareToIgnoreCase((getName2(paramArray.get(i + 1)))) > 0) {
+                    temp = paramArray.get(i);
+                    paramArray.set(i, paramArray.get(i + 1));
+                    paramArray.set(i + 1, temp);
+                    needSort = true;
+                }
+            }
+        } while (needSort);
+    }
+
+    static void initArrayRandom(ArrayList<Object> Gifts, int count) {
+        for (int i = 0; i < count; i++) {
+
+            int choose = (int) (Math.random() * 10);
+            switch (choose) {
+                case 0:
+                    Gifts.add(new Jpeg("Name" + (int) (Math.random() * 10)));
+                    break;
+                case 1:
+                    Gifts.add(new Mp3("Name" + (int) (Math.random() * 10)));
+                    break;
+                case 2:
+                    Gifts.add(new Txt("Name" + (int) (Math.random() * 10)));
+                    break;
+                case 3:
+                    Gifts.add(new Aster());
+                    break;
+                case 4:
+                    Gifts.add(new Charmomile());
+                    break;
+                case 5:
+                    Gifts.add(new Rose());
+                    break;
+                case 6:
+                    Gifts.add(new Tulip());
+                    break;
+                case 7:
+                    Gifts.add(new Guitar());
+                    break;
+                case 8:
+                    Gifts.add(new Piano());
+                    break;
+                case 9:
+                    Gifts.add(new Trumpet());
+                    break;
+                default:
+                    System.out.println("cant find " + choose);
+            }
+        }
+
     }
 }
